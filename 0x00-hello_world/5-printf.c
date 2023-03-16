08311ba4260b10c840a624fed0b5ad556fffa1cc
aminebouygdade <aminebouyg@gmail.com>
2023-03-16 10:55:59 -0700

    hihl

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+	/**
+	* main - Prints out a string to stdout.
+	* Return : 0 (Succes)
+	*/
+	int main(void) {
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+	}
