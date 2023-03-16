ebc8093f8d38deddd44fe1504608f9030158116a
aminebouygdade <aminebouyg@gmail.com>
2023-03-16 11:38:28 -0700

    last one

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,15 @@
-#include <stdio.h>
-	/**
-	* main - Prints out a string to stdout.
-	* Return : 0 (Succes)
-	*/
-	int main(void) {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-	printf("Size of a char: %lu byte(s) \n", (unsigned long)sizeof(a));
-	printf("Size of an int: %lu byte(s) \n", (unsigned long)sizeof(b));
-	printf("Size of a long int: %lu byte(s) \n", (unsigned long)sizeof(c));
-	printf("Size of a clong long int: %lu byte(s) \n", (unsigned long)sizeof(d));
-	printf("Size of a float: %lu byte(s) \n", (unsigned long)sizeof(f));
+#include<stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
-	}
+}
