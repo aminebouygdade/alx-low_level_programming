1ce4a1e8fbe55c268b10062db0ad5667b936ef8c
aminebouygdade <aminebouyg@gmail.com>
2023-03-22 16:24:58 -0700

    oil

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,13 @@
-#include "main.h"
-
+# include "main.h"
 /**
- * print_sign - prints the sign of a number
- * @n: the number to check
+ * print_sign-check the sign of a number.
  *
- * Return: 1 if n is greater than zero, 0 if n is zero, -1 if n is less than zero
+ * @n:Number whose sign is to be checked.
+ * Return:1-if n is greater than 0: 0- if n equals 0:-1-if n is less than 0
  */
+
+
+
 int print_sign(int n)
 {
 	if (n > 0)
@@ -13,15 +15,15 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-		_putchar('0');
-		return (0);
+		_putchar ('-');
+		return (-1);
 	}
 	else
 	{
-		_putchar('-');
-		return (-1);
+		_putchar('0');
+		return (0);
 	}
 }
 
