d20015cbb931296ed913e65745e58954bd09e5d1
aminebouygdade <aminebouyg@gmail.com>
2023-03-21 19:43:14 -0700

    hli

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,3 +17,21 @@ void times_table(void)
 				_putchar(k + '0');
 			}
 
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			} else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
+
