88dc467a236ac1da5d0c091320b3830a58053e36
aminebouygdade <aminebouyg@gmail.com>
2023-04-05 18:19:29 -0700

    li

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
- * function like puts();
+ * _puts_recursion - function like puts();
  * @s: input
  * Return: Always 0 (Success)
  */
+
 void _puts_recursion(char *s)
 {
 	if (*s)
