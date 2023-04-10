7a1d1a744b2abdd99df1e38b74045d17f1e1a269
aminebouygdade <aminebouyg@gmail.com>
2023-04-10 15:58:01 -0700

    li

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,14 +1,15 @@
 #include "main.h"
+#include <stddef.h>
+
 /**
  * _strchr - Entry point
  * @s: input
  * @c: input
- * Return:Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 char *_strchr(char *s, char c)
 {
 	int i = 0;
-
 
 	for (; s[i] >= '\0'; i++)
 	{
