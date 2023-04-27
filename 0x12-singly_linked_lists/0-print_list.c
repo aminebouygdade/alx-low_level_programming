40b757c96f4709dc2cc0a0bcf75820225b9b62fe
aminebouygdade <aminebouyg@gmail.com>
2023-04-27 06:54:37 -0700

    li

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - print the elemt list
+ * @h: pointer to the list
+ *
+ * Return: number of node
+ */
+size_t print_list(const list_t *h)
+{
+	size_t s = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		s++;
+	}
+	return (s);
+}
