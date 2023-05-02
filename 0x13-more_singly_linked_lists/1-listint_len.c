e83aeb4db1a3425a03f66ec2cc5955d8f567ec86
aminebouygdade <aminebouyg@gmail.com>
2023-05-02 12:18:28 -0700

    il

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint_len - return number of elem
+ * @h: linked list
+ *
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
+
+	return (num);
+}
+
