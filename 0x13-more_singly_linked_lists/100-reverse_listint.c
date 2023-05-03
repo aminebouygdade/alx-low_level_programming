0f738dc562603986bc195467a02bca9f129b7b90
aminebouygdade <aminebouyg@gmail.com>
2023-05-02 18:52:14 -0700

    il

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverses
+ * @head: P to first node
+ *
+ * Return: P to first node
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
+
+	while (*head)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+
+	*head = prev;
+
+	return (*head);
+}
