bd64d88126ba997b3193c5dbfe79eadeeaca06ee
aminebouygdade <aminebouyg@gmail.com>
2023-05-09 09:31:42 -0700

    il

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - create a file
+ * @filename: P to name of file
+ * @text_content: P to a string write file.
+ *
+ * Return: if function fails -1 or 1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
+
