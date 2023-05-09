a9c8eb237b77822e9c6033e5b8f7e8a9f4a34172
aminebouygdade <aminebouyg@gmail.com>
2023-05-09 09:16:59 -0700

    il

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - Read text file print STDOUT.
+ * @filename: text being read.
+ * @letters: number of letters
+ * Return: number of bytes 0 or filesname NULL
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
+
+	free(buf);
+	close(fd);
+	return (w);
+}
+
